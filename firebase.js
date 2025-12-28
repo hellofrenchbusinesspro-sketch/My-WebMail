1f06b48d2e2426059e7462d7aece726a93972d4b
hellofrenchbusinesspro-sketch <hello.frenchbusiness.pro@gmail.com>
2025-12-28 04:56:12 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebas
 import { getAuth } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAf0rz9mpUG2GpxyBeIyBvuHLHu-mANlSt",
-  authDomain: "movnembaii.firebaseapp.com",
-  projectId: "movnembaii",
-  storageBucket: "movnembaii.firebasestorage.app",
+  apiKey: "AIzaSyAf07zpmUPG26pxyBelY8VuHLUo-mANlSt",
+  authDomain: "monwebmail.firebaseapp.com",
+  projectId: "monwebmail",
+  storageBucket: "monwebmail.firebasestorage.app",
   messagingSenderId: "753446508336",
-  appId: "1:753446508336:web:638f226312374d79e32fed",
-  measurementId: "G-CPYXKPRQGW"
+  appId: "1:753446508336:web:638f236213274d79e32fed",
+  measurementId: "G-CPYXPKRQGW"
 };
 
 export const app = initializeApp(firebaseConfig);
